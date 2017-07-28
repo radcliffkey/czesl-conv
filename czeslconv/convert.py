6d9014eb37e8326374eb1d5e53a2f9887c4386d6
Radoslav Klic <radoslav.klic@gmail.com>
2017-07-28 18:15:18 +0200

    process links between tokens

--- czeslconv/convert.py
+++ czeslconv/convert.py
@@ -13,7 +13,7 @@ import bs4
 
 from collections import defaultdict
 from os import DirEntry
-from typing import Dict, Iterable, List, Mapping, NamedTuple
+from typing import Any, Dict, Iterable, List, Mapping, NamedTuple, Sequence, Union
 
 from bs4 import BeautifulSoup
 
@@ -32,6 +32,67 @@ class MetaXml(NamedTuple):
     bxml: str
 
 
+class Morph(NamedTuple):
+    lemma: str
+    tags: Sequence[str]
+
+
+class WToken(NamedTuple):
+    text: str
+
+
+class AToken(NamedTuple):
+    text: str
+    morphs: Sequence[Morph]
+
+
+class BToken(NamedTuple):
+    text: str
+    morph: Morph
+
+
+class ErrorData(NamedTuple):
+    tags: Sequence[str]
+    links: Sequence[str]
+
+    @staticmethod
+    def fromTag(error: bs4.Tag) -> 'ErrorData':
+        tags = [t.string for t in error.find_all(name='tag')]
+        links = [l.string for l in error.find_all(name='link')]
+        return ErrorData(tags=tags, links=links)
+
+
+class AnnotToken:
+    """
+    Annotated token with link to other layers
+    """
+
+    def __init__(self,
+            baseToken: Union[WToken, AToken, BToken],
+            *,
+            layer:str = None,
+            sentenceId: str=None,
+            linkIdsLower: Iterable[str]=None,
+            linkIdsHigher: Iterable[str]=None,
+            errors: Iterable[ErrorData]=None,
+            linksLower: Iterable['AnnotToken']=None,
+            linksHigher: Iterable['AnnotToken']=None
+    ):
+        self.baseToken = baseToken
+        self.layer = layer
+        self.sentenceId = sentenceId
+        self.linkIdsLower = linkIdsLower
+        self.linkIdsHigher = linkIdsHigher
+        self.errors = errors
+        self.linksLower = linksLower
+        self.linksHigher = linksHigher
+
+
+class DeletionToken(NamedTuple):
+    fromId: str
+    errors: Sequence[ErrorData]
+
+
 def readFile(fileName: str) -> str:
     with open(fileName, 'r', encoding='utf-8') as f:
         return f.read()
@@ -44,6 +105,15 @@ def readMetaFile(metaFile: MetaFile) -> MetaXml:
         axml=readFile(metaFile.afile),
         bxml=readFile(metaFile.bfile)
     )
+
+
+def revertMapping(mapping: Mapping[Any, Iterable[Any]]) -> Mapping[Any, List[Any]]:
+    revertedMap = defaultdict(list)
+    for k, vals in mapping.items():
+        for v in vals:
+            revertedMap[v].append(k)
+
+    return revertedMap
 
 
 def processFileGroups(baseNameToPaths: Mapping[str, Dict[str, str]]) -> Iterable[MetaFile]:
@@ -93,6 +163,49 @@ def getMetaFilesFromDir(dirName: str) -> Iterable[MetaFile]:
     yield from processFileGroups(baseNameToPaths)
 
 
+def createLinkedLayers(wPara: bs4.Tag, aPara: bs4.Tag, bPara: bs4.Tag):
+    idMapWA = defaultdict(list)
+    idMapAB = defaultdict(list)
+
+    for aw in aPara.find_all(name='w'):
+        awId = aw['id']
+        if aw.edge:
+            fromIds = [inEdge.string.split('#', maxsplit=1)[1] for inEdge in aw.edge.find_all(name='from')]
+            toIds = [awId] + [outEdge.string for outEdge in aw.edge.find_all(name='to')]
+            for fromId in fromIds:
+                idMapWA[fromId].extend(toIds)
+
+    idToDelW = findDeletions(aPara)
+
+    for bw in bPara.find_all(name='w'):
+        bwId = bw['id']
+        if bw.edge:
+            fromIds = [inEdge.string.split('#', maxsplit=1)[1] for inEdge in bw.edge.find_all(name='from')]
+            toIds = [bwId] + [outEdge.string for outEdge in bw.edge.find_all(name='to')]
+            for fromId in fromIds:
+                idMapAB[fromId].extend(toIds)
+
+    idToDelA = findDeletions(bPara)
+
+    idMapAW = revertMapping(idMapWA)
+    idMapBA = revertMapping(idMapAB)
+
+
+def findDeletions(paragraph: bs4.Tag) -> Dict[str, DeletionToken]:
+    idToDel = {}
+    for delEdge in paragraph.find_all(name='edge', recursive=False):
+        if delEdge.to:
+            print(f'Unexpected non-deletion edge directly under <para>: {delEdge}', file=sys.stderr)
+            continue
+        fromIds = [inEdge.string.split('#', maxsplit=1)[1] for inEdge in delEdge.find_all(name='from')]
+        errors = [ErrorData.fromTag(err) for err in delEdge.find_all(name='error')]
+
+        for fromId in fromIds:
+            idToDel[fromId] = DeletionToken(fromId=fromId, errors=errors)
+
+    return idToDel
+
+
 def paraToVert(bPara: bs4.Tag, aDoc: bs4.Tag, wDoc: bs4.Tag) -> str:
     bParaId = bPara['id']
     commonId = bParaId.split('-', maxsplit=1)[1]
@@ -103,7 +216,7 @@ def paraToVert(bPara: bs4.Tag, aDoc: bs4.Tag, wDoc: bs4.Tag) -> str:
     wParaId = aPara['lowerpara.rf'].split('#', maxsplit=1)[1]
     wPara = wDoc.find(name='para', id=wParaId, recursive=False)
 
-
+    createLinkedLayers(wPara, aPara, bPara)
 
     vertBuffer: List[str] = []
 
